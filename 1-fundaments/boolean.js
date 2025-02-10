e383125364a54d8c5ad384a39cb608e1db261236
caio f f <caiofkreitas@gmail.com>
2025-02-10 10:07:28 -0300

    Create boolean.js

---
+++ 1-fundaments/boolean.js
@@ -0,0 +1,29 @@
+isAtive = true;
+
+//true
+console.log(isAtive);
+
+//false (!)
+console.log(!isAtive);
+
+//1 = true -> !! = true/false/true
+isAtive = 1;
+console.log(!!isAtive);
+
+//os outros "verdadeiros"
+console.log('Os de "verdade": ');
+console.log(!!3);
+console.log(!!-1);
+console.log(!!' ');
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+
+//o falso
+console.log('os falsos: ')
+console.log(!!0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+
