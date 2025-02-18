e7449ecb1f9f5a57b103a9bfa344b5e8e9c85929
caio f f <caiofkreitas@gmail.com>
2025-02-18 08:56:20 -0300

    Update ex6.js

--- 2-functions/exercises/ex6.js
+++ 2-functions/exercises/ex6.js
@@ -2,3 +2,21 @@
 primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
 retornará o valor da aplicação sob o regime de juros compostos.*/
 
+//juros simples
+function simples(capIni, taxaJuro, tempo){
+    let juros = capIni*taxaJuro*tempo;
+    let montante = juros + capIni;
+    console.log('O valor em juros simples é de ' + montante)
+}
+
+
+//juros compostos
+function compostos(capIni, taxaJuro, tempo){
+    taxaJuro = 1+taxaJuro;
+    let juros = (capIni)*(Math.pow(taxaJuro, tempo));
+    let montante = juros + capIni;
+    console.log('O valor em juros compostos é de ' + montante)
+}
+
+simples(151,0.02,12);
+compostos(151,0.02,12);
