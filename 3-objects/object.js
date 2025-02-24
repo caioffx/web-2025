92569540bca12e60201a918ee8d53f69cd061746
caio f f <caiofkreitas@gmail.com>
2025-02-24 12:16:45 -0300

    Update object.js

--- 3-objects/object.js
+++ 3-objects/object.js
@@ -28,4 +28,28 @@ const newUser = {
     }
 }
 
+newUser.login = 'cvengenz';
+newUser.password = '123456';
+
+
 console.log(newUser)
+
+//objeto constante, porém é possível modificar tudo o que estiver 'dentro' do objeto, como 'pessoa.nome'
+const pessoa = {
+    nome: 'João',
+}
+console.log(pessoa);
+
+pessoa.nome = 'Pedro';
+
+console.log(pessoa);
+
+//object.freeze faz com que além do objeto seja inalterável (por ser constante), todas chaves dentro passam a ser constantes também.
+Object.freeze(pessoa);
+pessoa.nome = 'Neymar';
+console.log(pessoa);
+
+//vale lembrar que o objeto passa a não poder receber nenhuma nova chave dentro dele. Além disso, é impossível deletar qualquer informação.
+pessoa.end = 'Rua dos Bobos';
+delete pessoa.nome;
+console.log(pessoa);
